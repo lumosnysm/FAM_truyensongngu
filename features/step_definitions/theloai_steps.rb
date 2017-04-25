edc62c75dab2be4e3eca9f996bc36fd5a3f40004
Dieu Linh <linhhqnk97@gmail.com>
2017-04-25 09:13:11 +0700

    some feature

---
+++ features/step_definitions/theloai_steps.rb
@@ -0,0 +1,41 @@
+And(/^I should see list stories$/) do
+
+end
+
+Given(/^I am on "([^"]*)" page$/) do |arg|
+end
+
+
+Given(/^I move the mouse on "([^"]*)" story$/) do |arg|
+end
+
+Then(/^I should see "([^"]*)" story's color change$/) do |arg|
+end
+
+And(/^I should see "([^"]*)" underlined$/) do |arg|
+end
+
+And(/^I should see a part of "([^"]*)" story content$/) do |arg|
+end
+
+Given(/^I on picture of "([^"]*)" story$/) do |arg|
+end
+
+Then(/^I should see a part of "([^"]*)" story's content$/) do |arg|
+end
+
+Given(/^I on "([^"]*)" story$/) do |arg|
+end
+
+When(/^I click on the "([^"]*)"$/) do |arg|
+end
+
+Then(/^I should on "([^"]*)" story's content page$/) do |arg|
+end
+
+When(/^I click on picture of "([^"]*)" story$/) do |arg|
+end
+
+When(/^I move the mouse on picture of "([^"]*)" story$/) do |arg|
+
+end
