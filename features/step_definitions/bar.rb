84849d24b9a0b7dcb55ccf91cc86acd96c11501a
Ngô Hải Yến <yourgod1989@gmail.com>
2017-04-27 22:10:13 +0700

    add test and fix view

--- features/step_definitions/bar.rb
+++ features/step_definitions/bar.rb
@@ -1,5 +1,5 @@
 Given(/^I am on any page$/) do
-  
+   
 end
 
 When(/^I click on "([^"]*)"$/) do |arg1|
@@ -7,7 +7,7 @@ When(/^I click on "([^"]*)"$/) do |arg1|
 end
 
 Then(/^I can see "([^"]*)"$/) do |arg|
-
+ 
 end
 
 Then(/^I should see the pictures of the home page$/) do
