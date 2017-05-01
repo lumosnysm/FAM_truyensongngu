d31cdceee14e75535029c69334a145c83c15236c
lumosnysm <lumosnysm@gmail.com>
2017-05-01 21:30:06 +0700

    chuc nang danh gia ( chua hoang thien )

--- app/controllers/truyens_controller.rb
+++ app/controllers/truyens_controller.rb
@@ -4,6 +4,15 @@ class TruyensController < ApplicationController
     @truyen = Truyen.find(params[:id])
     @binhluans=@truyen.binhluans.order("updated_at DESC")
     @binhluan = Binhluan.new
+    @binhluan_num=@truyen.binhluans.count
+    @rating=Rating.new
+    @rated=false
+    if @truyen.ratings.exists?(:user_id => current_user.try(:id))
+      @rated=true
+      @user_rate= @truyen.ratings.find_by_user_id(current_user.id).rate
+    end
+    @rating_num=@truyen.ratings.count
+    @average_rate=@truyen.ratings.average(:rate)
   end
   def all
     @theloais=Theloai.all
